1575a81d4253197ef061460e9e58e82735f62419
Paiva6002 <paivag1109@gmail.com>
2023-06-22 01:11:20 -0300

    Tentativa lógica

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,52 @@
 const imgs = document.querySelectorAll('img');
-const cols = document.querySelectorAll('.col-4');
+const cols = document.querySelectorAll('.col-2');
 
 
 let imagens = ['./assets/img/1.png', './assets/img/2.png', './assets/img/3.png', './assets/img/4.png', './assets/img/5.png', './assets/img/6.png', './assets/img/7.png', './assets/img/8.png', './assets/img/9.png', './assets/img/10.png', './assets/img/11.png', './assets/img/12.png']
 
-let count = [];
-let = [];
-let igual = 0
+let randomN = 0;
+let numbers = []
+let nao = []
 
+function randomNumber() {
+  randomN = parseInt(Math.random() * 5);
+}
 
+function arrayRandomNumber() {
+  for (i=0;i<5;i++) {
+    randomNumber();
 
-cols.forEach(col => {
-  let randomN = parseInt(Math.random() * 11);
-  count[col.id] = randomN
-
-  imagens.forEach(imagem => {
-    for(i=0;i<12;i++) {
-      if(imagem == imagens[i]) {
-        igual ++
-      }
-      if (igual == 2) {
-        imagens[i] = ''
-        
-        igual = 0
-      }
-
-
+    numbers.push(randomN);
   }
+
+  numbers.forEach(n => {
+    for (i=0;i<5;i++) {
+      if (n != i) {
+        numbers.splice(i, 1);
+      } else {
+
+      }
+    }
   });
 
-  imgs[col.id].setAttribute('src', imagens[randomN])
-})
 
 
 
-console.log(imagens)
 
+
+}
+
+
+
+
+
+arrayRandomNumber()
+console.log(numbers);
 
 // cols.forEach(col => {
-//   col.addEventListener('click', () => {
-//     let randomN = parseInt(Math.random() * 11);
-    
 
-//     imgs[col.id].setAttribute('src', imagens[randomN]);
-//     imgs.forEach(img => {
-      
-//     });
-//   })
-// });
 
+//   imgs[randomN].setAttribute('src', imagens[randomN]);
+
+// })
 
