15f1d37a6fb69b17b83169b41cba85cab8e96a16
Paiva6002 <paivag1109@gmail.com>
2023-06-24 20:17:19 -0300

    Primeira parte lógica feita

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,64 @@
-const imgs = document.querySelectorAll('img');
+const imgs = document.querySelectorAll('.img');
+const cards = document.querySelectorAll('.card');
 const cols = document.querySelectorAll('.col-2');
+const btn = document.querySelector('.btn')
+
+let imagens = ['./assets/img/1.png', './assets/img/2.png', './assets/img/3.png', './assets/img/4.png', './assets/img/5.png', './assets/img/6.png', './assets/img/7.png', './assets/img/8.png', './assets/img/9.png', './assets/img/10.png', './assets/img/11.png', './assets/img/12.png', './assets/img/1.png', './assets/img/2.png', './assets/img/3.png', './assets/img/4.png', './assets/img/5.png', './assets/img/6.png', './assets/img/7.png', './assets/img/8.png', './assets/img/9.png', './assets/img/10.png', './assets/img/11.png', './assets/img/12.png']
 
 
-let imagens = ['./assets/img/1.png', './assets/img/2.png', './assets/img/3.png', './assets/img/4.png', './assets/img/5.png', './assets/img/6.png', './assets/img/7.png', './assets/img/8.png', './assets/img/9.png', './assets/img/10.png', './assets/img/11.png', './assets/img/12.png']
+let nRandom;
+let numbers = [];
+let cardsGalery = [];
 
-let randomN = 0;
-let numbers = []
-let nao = []
 
-function randomNumber() {
-  randomN = parseInt(Math.random() * 5);
-}
-
-function arrayRandomNumber() {
-  for (i=0;i<5;i++) {
-    randomNumber();
-
-    numbers.push(randomN);
-  }
-
+function arrayRandom() {
+  nRandom = parseInt(Math.random() * 24)
+  
   numbers.forEach(n => {
-    for (i=0;i<5;i++) {
-      if (n != i) {
-        numbers.splice(i, 1);
-      } else {
-
-      }
+    if (nRandom == n) {
+      arrayRandom()
+      n = nRandom
     }
   });
-
-
-
-
-
-
+  
+  if (numbers.length == 24) {
+    return
+  }
+  numbers.push(nRandom);
+  if (numbers.length < 24) {
+    arrayRandom();
+  }
 }
 
+arrayRandom();
 
 
 
+numbers.forEach(n => {
+  divImg = document.createElement('img');
+  cards[numbers[n]].appendChild(divImg);
+  
 
-arrayRandomNumber()
-console.log(numbers);
+  divImg.setAttribute('src',  imagens[n]);
 
-// cols.forEach(col => {
+  cardsGalery.push(imagens[n])
+});
+
+  
+  console.log();
 
 
-//   imgs[randomN].setAttribute('src', imagens[randomN]);
+btn.addEventListener('click', () => {
+  for(i=0;i<24;i++) {
+    cards[i].classList.add('d-none');
+  }
 
-// })
+  cols.forEach(col => {
+    col.addEventListener('click', () => {
+      cards[col.id].classList.remove('d-none')
+      console.log(cardsGalery[col.id])
+    })
+  });
+  
+})
 
