bcb2228bb9065495a26b572e0f2fa21d0c4addeb
Paiva6002 <paivag1109@gmail.com>
2023-06-27 17:19:02 -0300

    Comentários concluídos

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,104 +66,110 @@ btn.addEventListener('click', () => { // Função de clique no botão 'Jogar', t
     cards[i].classList.add('d-none');
   }
 
-  let click = 0; // Variável criada 
-  let clickCount = 0;
-  let lister = 1;
-  let clicker = true;
-  let clickerCheck;
-  let imgNone;
-  let colsGalery = [];
-  let colSecondary;
+  let click = 0; // Guarda valor da url da primeira imagem
+  let clickCount = 0; // Guarda valor da url da segunda imagem
+  let lister = 1; // Variavel criada para possibilitar que a troca de dados entre variáveis possa exister
+  let clicker = true; // Possibilita que a mesma carta não seja clicada mais de uma vez
+  let clickerCheck; // Armazena a primeira coluna clicada
+  let imgNone; // Armazenar a primeira imagem 
+  let colsGalery = []; // Array criado para armazenar as colunas que já possuem um par de cards 
+  let colSecondary; // Var criada a primeira coluna que foi clicada
 
-  const life = document.querySelector('.life');
-  const points = document.querySelector('.points');
+  const life = document.querySelector('.life'); // Pego o elemento de vida que está no html
+  const points = document.querySelector('.points'); // Pego o elemento de acertos que está no html
 
-  points.innerHTML = 'X0' + p1.points;
-  life.innerHTML = '00000' + p1.life;
+  points.innerHTML = 'X0' + p1.points; // Exibo no documento os acertos
+  life.innerHTML = '00000' + p1.life; // Exibo no documento os pontos de vida
 
-  cols.forEach(col => {
+  cols.forEach(col => { // Para cada elemento do array cols, dever ser feito...
 
-    col.addEventListener('click', () => {
+    col.addEventListener('click', () => { // Irá adicionar um evento de click para cada elemento do array cols
 
       colsGalery.forEach(colGale => {
 
-        if (col == colGale) {
-          col.removeEventListener('click');
+        if (col == colGale) { // Removerá o evento de click se a carta (imagem) já estiver sendo exibida, se a carta já possuir seu par e está sendo exibida no documento
+          col.removeEventListener('click'); // Remove evento de click
         }
       });
 
-      if (clicker == true) {
-        clicker = false;
+      if (clicker == true) { // Condição feita para saber se o card já foi clicado ou não, ou seja, não é possível clicar mais de uma vez no elemento 
+        clicker = false; // Resetar o valor do clicker, para que logo em seguida o if possa ser executado
 
-        cards[col.id].classList.remove('d-none');
-        imgs[col.id].classList.remove('d-none');
+        cards[col.id].classList.remove('d-none'); // Remove o display none para que a coluna apareça
+        imgs[col.id].classList.remove('d-none'); // Remove o display none para que o elemento imagem apareça
 
-        function settime() {
-          if (click != clickCount) {
+        function settime() { // Função criada para colocar um atraso nos eventos
+          if (click != clickCount) { // Verfica se as duas imagens que foram exibidas logo após o click, são diferentes, se sim..
 
-            imgNone.classList.add('d-none');
-            imgs[col.id].classList.add('d-none');
+            imgNone.classList.add('d-none'); // Esconde a primeira imagem
+            imgs[col.id].classList.add('d-none'); // Esconde a segunda imagem
 
-            p1.life = p1.life - 1;
-            life.innerHTML = '00000' + p1.life;
+            p1.life = p1.life - 1; // Se as imagens forem diferentes, isso significa que ele errou, então consequentemente ele acaba perdendo pontos de vida
+            life.innerHTML = '00000' + p1.life; // É exibido os pontos de vida no documento com seu valor já atualizado
 
+            if (p1.life == 0) { // Quando seus pontos de vida chegarem a 0...
+              container.classList.add('d-none'); // Esconde o elemento container, que é o elemento principal
+              containerMsg.classList.remove('d-none'); // Remove display none do elemento, assim o tornando visível novamente
+              msg.classList.remove('d-none'); // Remove display none do elemento, assim o tornando visível novamente
 
-            if (p1.life == 0) {
-              container.classList.add('d-none');
-              containerMsg.classList.remove('d-none');
-              msg.classList.remove('d-none');
+              msg.innerHTML = "GAME OVER"; // Quando seus pontos de vida chegarem a 0, será exibido a mensagem de Game Over
 
-              msg.innerHTML = "GAME OVER";
-
-              life.innerHTML = p1.life = '00000' + 3
+              life.innerHTML = p1.life = '00000' + 3; // É exibido no documento a quantidade de pontos de vida que o jogador possui enquando ele joga
             }
           }
 
+          if (click == clickCount) { // Verfica se as duas imagens que foram exibidas logo após o click, são iguais, se sim..
+            colsGalery.push(col, colSecondary); // Envia as colunas que foram clicadas para o array colsGalery, esse array será usado para remover o evento de click se a carta já estiver sendo exibida, se a carta já possuir seu par e está sendo exibida no documento
 
-          if (click == clickCount) {
-            colsGalery.push(col, colSecondary);
+            p1.points = p1.points + 1; // Soma mais um ponto para o jogador, já que ele achou um par
+            points.innerHTML = 'X0' + p1.points; // Exibi o valor atualizado dos acertos no documento
 
-            p1.points = p1.points + 1;
-            points.innerHTML = 'X0' + p1.points;
+            if (p1.points == 12) { // Quando seus acertos chegarem a 12...
+              container.classList.add('d-none'); // Esconde o elemento container, que é o elemento principal
+              containerMsg.classList.remove('d-none'); // Remove display none do elemento, assim o tornando visível novamente
+              msg.classList.remove('d-none'); // Remove display none do elemento, assim o tornando visível novamente
 
-            if (p1.points == 12) {
-              container.classList.add('d-none');
-              containerMsg.classList.remove('d-none')
-              msg.classList.remove('d-none');
+              msg.innerHTML = "CONGRATULATIONS"; // Quando seus acertos chegarem a 12, será exibido a mensagem de Congratulations
 
-              msg.innerHTML = "CONGRATULATIONS";
-              
 
-              life.innerHTML = p1.life = '00000' + 0
+              points.innerHTML = p1.points = '00000' + 0; // É exibido no documento a quantidade de acertos que o jogador acertou enquando ele joga e vai ganhando
             }
           }
+
         }
-        clickCount = click;
+        clickCount = click; // Guarda a imagem a url da imagem que foi clicada, para que assim possa ser verificado que se os pares são iguais ou não
 
-        if (lister == 2) {
-          setTimeout(settime, 150);
-          lister = 0;
+        if (lister == 2) { // Quando lister for igual a 2, executar linhas que estão dentro desse if
+          if (clickerCheck != col.id) {
+            setTimeout(settime, 150); // Atraso de 150 milissegundos
+          }
+          if (clickerCheck == col.id) { // Se for clicado na mesma card duas vezes...
+            cards[col.id].classList.add('d-none'); // Card será escondida
+            imgs[col.id].classList.add('d-none'); // Card será escondida
+          }
+
+          lister = 0; // Reinicia o valor do lister, para que assim o loop possa rodar novamente 
         }
 
-        if (lister == 1) {
-          imgNone = imgs[col.id];
+        if (lister == 1) { // Quando lister for igual a 1, executar linhas que estão dentro desse if
+          imgNone = imgs[col.id]; // imgNone guarda a primeira imagem
 
-          colSecondary = col;
+          colSecondary = col; // colSecondary recebe o valor da coluna que foi clicada
         }
-        click = imagens[imgs[col.id].id];
+        click = imagens[imgs[col.id].id]; // click receve o valor da url da imagem
 
-        lister++;
+        lister++; // Lister irá receber + 1, que irá somar com o seu valor
       }
 
-      if (clickerCheck == col.id) {
-        clicker = false;
+      // Sistema de restrição , feito para que o usuário não possa clicar mas de uma vez no card 
+      if (clickerCheck == col.id) {  // Se clicker for igual o id da coluna que foi clicada
+        clicker = false; // clicker é igual a false
       }
-      if (clickerCheck != col.id) {
-        clicker = true;
+      if (clickerCheck != col.id) { // Se clicker for diferente o id da coluna que foi clicada
+        clicker = true; // clicker é igual a true
       }
 
-      clickerCheck = col.id;
+      clickerCheck = col.id; // clickerCheck é igual o id da coluna clicada
     });
   });
 });
-
